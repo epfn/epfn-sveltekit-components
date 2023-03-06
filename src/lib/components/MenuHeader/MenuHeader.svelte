33ddb607d4eb9d4894d3c2278063660fac8804ac
Aleksei Epifantsev <alex@lahtihosting.fi>
2023-03-06 23:55:15 +0400

    feat: add MenuHeader component

---
+++ src/lib/components/MenuHeader/MenuHeader.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let links: { href: string; label: string }[];
+
+	$: pathname = $page.url.pathname;
+</script>
+
+<ul>
+	{#each links as link}
+		<li>
+			<a href={link.href} aria-current={link.href === pathname ? 'page' : undefined}
+				>{link.label}
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	a {
+		padding: 0.5rem;
+	}
+
+	a:hover,
+	a[aria-current='page'] {
+		text-underline-position: under;
+		text-decoration-line: underline;
+		text-decoration-thickness: var(--underline-thickness, 2px);
+		text-underline-offset: var(--underline-offset, 20px);
+		text-decoration-color: var(--underline-color, gray);
+	}
+</style>
---
+++ src/lib/components/MenuHeader/index.ts
@@ -0,0 +1,2 @@
+export { default as MenuHeader } from './MenuHeader.svelte';
+export { default as default } from './MenuHeader.svelte';
