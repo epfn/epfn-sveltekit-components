ada058d9313e4890c1ae4242eb488f07364c8166
Aleksei Epifantsev <alex@lahtihosting.fi>
2023-03-09 21:37:13 +0400

    fix: add imports for Alerts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,5 @@ export { default as Drawer } from './components/Drawer.svelte';
 export { default as MenuDrawer } from './components/MenuDrawer.svelte';
 export { default as MenuHeader } from './components/MenuHeader.svelte';
 export { default as Spinner } from './components/Spinner.svelte';
+export { default as Alerts } from './components/Alerts.svelte';
+export type { AlertItem as AlertItem } from './components/Alerts.svelte';
